334e9420b182bcabd0bc9e7ef478c27bdca03370
Gaurav Joseph <gaurav.joseph@gmail.com>
2014-03-06 05:55:17 -0500

    added database classes, options tables, functionalities and connected to the plugin

--- resulter.php
+++ resulter.php
@@ -23,17 +23,76 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
+require("resulter_db_class.php");
+require("resulter_options.php");
+require("resulter_goodies.php");
+
+
 function resulter_handler($atts){
-    extract( shortcode_atts( array(
-		'_input_id' => 'none',
-	), $atts ) );
-	
-	if($_input_id=="none"){
-	    return "<h1>This user is invalid</h1>You have reached an unintended page. Please use the navigation links for browsing our site.";
+	$query_string_rs_user = "SELECT * FROM `students` WHERE `regn`='%s' LIMIT 1";
+	$query_string_rs_resl = "SELECT * FROM `batchresult%s` WHERE `studentid`='%s' ORDER BY `semester`";
+	ob_start();
+	echo('<div><form method="POST" action="' . $_SERVER['REQUEST_URI'] . '">');
+	echo('<input type="text" name="input-id"><input type="submit">');
+	echo('</form></div>');
+
+	if(!isset($_POST["input-id"])){ return ob_get_clean(); }
+	if((!empty($_POST["input-id"])) && preg_match("/[ptPT][0-9]+\/[0-9]+/", $_POST["input-id"], $matches)){
+	        $db = new Resulter_Database(
+	        	get_option('resulter_db_hostname'),
+		        get_option('resulter_db_username'),
+		        get_option('resulter_db_password'));
+	        if($db->Connect()){
+		        if(!$db->Execute(
+		        	get_option('resulter_db_database'),
+		        	sprintf(query_string_rs_user, $db->SanitizeString($matches[0])))){
+				echo("We are having some trouble, please try later!");
+		        	return ob_get_clean();
+		        }
+		        $res = $db->FetchResult();
+		        if(empty($res[0]["name"])){
+		        	echo("There are no entries by this name");
+		        	return ob_get_clean();
+		        }
+		        $student = new stdClass();
+		        $student->details = $res[0];
+		        $db->Execute(get_option('resulter_db_hostname'), sprintf(query_string_rs_resl, $res[0]["year"], $res[0]["studentid"]));
+		        $student->results = $db->FetchResult();
+		        //return $student;
+		        echo('<h2>Student Details</h2>');
+			echo('<table>');
+			echo('<tr>');
+			echo('	<td rowspan="4" style="padding:0;padding-right: 15px;">');
+			echo('		<img src="/app/data/images/students/' . str_replace("/", "-", $student->details["regn"]) . '.jpg" alt="' . $student->details["name"] . '" title="' . $student->details["name"] . '">');
+			echo('	</td>');
+			echo('	<td>Name:</td><td style="padding-left:15px;">' . $student->details["name"] . '</td>');
+			echo('</tr>');
+			echo('<tr>');
+			echo('	<td>Congregation:</td><td style="padding-left:15px;">' . $student->details["congregation"] . '</td>');
+			echo('</tr>');
+			echo('<tr>');
+			echo('	<td>Section:</td>');
+			echo('	<td style="padding-left:15px;">' . ($student->details["section"]=="P")?"Philosophy":"Theology" . '</td>');
+			echo('</tr>');
+			echo('<tr>');
+			echo('	<td>Register No.:</td><td style="padding-left:15px;">' . $student->details["regn"] . '</td>');
+			echo('</tr>');
+			echo('</table><br>');
+			echo('<h2>Results</h2>');
+			if($student->results)
+				echo(convertCrapResultsToTables($student->results));
+			else
+				echo("Sorry! No results available at this time");
+		}else
+			echo("We are having some database problems. Come by us again later.");
+	}else{
+		echo("Please enter the user id in the form: <strong>P12/208</strong>");
 	}
-}
+	return ob_get_clean();
+
+} //end_main
  
 add_shortcode("resulter-show", "resulter_handler");
- 
+
 ?>
